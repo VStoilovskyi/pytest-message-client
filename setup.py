23e8909d8319560e6135cc8a95b33cf87240af29
Stoilovskyi Vadym <vadim.stojlovskij@murka.com>
2022-01-05 15:09:22 +0200

    Define long description content type

--- setup.py
+++ setup.py
@@ -20,6 +20,7 @@ setup(
     name='pytest-message',
     version=__version__,
     description='Pytest plugin for sending report message of marked tests execution',
+    long_description_content_type="text/markdown",
     long_description=read_file('README.md'),
     author_email='vadym.stoilovskyi@gmail.com',
     url='https://github.com/VStoilovskyi/pytest-message',
